f1efd2f44d33d0fd75a02b0fa861979528528748
Вероника Емельянова <veronikaemelyanova1994@gmail.com>
2018-12-21 19:08:50 +0300

    Задание №5: Создать базовый класс для тестовых классов

---
+++ addressbook-web-tests/addressbook-web-tests/ContactRemovalTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace addressbook_web_tests
+{
+    [TestFixture]
+    public class ContactRemovalTests : TestBase
+    {
+       [Test]
+        public void ContactRemovalTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToContactsPage();
+            SelectContact(1);
+            RemoveContact();
+            SubmitRemoveContact();
+        }
+    }
+}
