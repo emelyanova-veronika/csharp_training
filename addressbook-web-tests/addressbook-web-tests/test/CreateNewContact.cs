e0121245031719500e5aaa4bfc4b3e518102dfb2
Вероника Емельянова <veronikaemelyanova1994@outlook.com>
2019-02-02 12:07:35 +0300

    Лекция 6.1. Чтение и запись текстовых файлов

--- addressbook-web-tests/addressbook-web-tests/test/CreateNewContact.cs
+++ addressbook-web-tests/addressbook-web-tests/test/CreateNewContact.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -19,8 +20,20 @@ namespace addressbook_web_tests
             }
             return contacts;
         }
-        
-        [Test,TestCaseSource("RandomContactDataProvider")]
+
+        public static IEnumerable<ContactData> ContactDataFromFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            string[] lines = File.ReadAllLines(@"contact.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                contacts.Add(new ContactData(parts[0], parts[1]));
+            }
+            return contacts;
+        }
+
+        [Test,TestCaseSource("ContactDataFromFile")]
         public void ContactCreationtTest(ContactData contact)
         {
             List<ContactData> oldContacts = app.Contacts.GetContactList();
